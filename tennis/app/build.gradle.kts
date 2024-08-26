ddb2a0b74754ae579cad9d3bcfa59f7635d4660d
kangminhee <minhee489@naver.com>
2024-08-27 04:40:06 +0900

    hmm

--- tennis/app/build.gradle.kts
+++ tennis/app/build.gradle.kts
@@ -72,8 +72,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("com.github.bumptech.glide:glide:4.15.1")
-
-    implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
 
 }
