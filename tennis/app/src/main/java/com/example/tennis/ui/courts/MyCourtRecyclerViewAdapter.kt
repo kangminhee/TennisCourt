317f27511bdc69adae42d841f0d16012aea10ff9
kangminhee <minhee489@naver.com>
2024-08-27 11:37:36 +0900

    중복된 테니스 코트 하나만 보이기

--- tennis/app/src/main/java/com/example/tennis/ui/courts/MyCourtRecyclerViewAdapter.kt
+++ tennis/app/src/main/java/com/example/tennis/ui/courts/MyCourtRecyclerViewAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.tennis.ui.courts
 
+import android.content.Intent
+import android.net.Uri
+
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import com.example.tennis.R
@@ -33,9 +37,9 @@ class MyCourtRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = places[position]
         holder.nameView.text = item.place
-        holder.areaView.text = item.area
+        holder.areaView.text = item.id
 
-        val isStarred = SharedPreferencesHelper.isCourtStarred(context, item.id)
+        val isStarred = SharedPreferencesHelper.isCourtStarred(context, item.place)
         holder.starButton.setImageResource(if (isStarred) R.drawable.ic_notifications_black_24dp else R.drawable.ic_notifications_white_24dp)
 
         // 별 모양 버튼 클릭 시 즐겨찾기 추가/제거 로직
@@ -43,15 +47,17 @@ class MyCourtRecyclerViewAdapter(
             val newStarredState = !isStarred
 
             if (newStarredState) {
-                SharedPreferencesHelper.addCourtToStars(context, item.id)
+                SharedPreferencesHelper.addCourtToStars(context, item.place)
 //                holder.starButton.setImageResource(R.drawable.ic_notifications_black_24dp)
             } else {
-                SharedPreferencesHelper.removeCourtFromStars(context, item.id)
+                SharedPreferencesHelper.removeCourtFromStars(context, item.place)
 //                holder.starButton.setImageResource(R.drawable.ic_notifications_white_24dp)
             }
             notifyItemChanged(position)
             notifyDataSetChanged()
         }
+
+//        Intent browserIntent = new Intent(i)
     }
 
     override fun getItemCount(): Int = places.size
@@ -61,6 +67,7 @@ class MyCourtRecyclerViewAdapter(
         val nameView: TextView = binding.textCourtName
         val areaView: TextView = binding.textCourtArea
         val starButton: ImageButton = binding.starBtn
+        val reserveButton: Button = binding.courtBtnReserv
 
 
 //        override fun toString(): String {
